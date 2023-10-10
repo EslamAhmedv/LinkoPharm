012dd67df94cb8a0f32b4b5bee80135a11e28213
nadaelamir101 <125657577+nadaelamir101@users.noreply.github.com>
2023-10-10 21:55:57 +0200

    homepage

---
+++ homepage.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="homepage.css">
+    
+</head>
+<body>
+ <div class="hero">
+    <nav>
+        <img src="images/logo.png" class="logo">
+        <ul>
+            <li><a href="#">Wishlist</a></li>
+            <li><a href="#">Cart</a></li>
+            <li><a href="#">Profile</a></li>
+        </ul>
+        <div>
+            <a href="#" class="login-btn">Log in</a>
+            <a href="#" class="btn">Download app</a>
+        </div>
+    </nav>
+    <div class="content">
+        <h1>Stay Home <br> & Order Online</h1>
+        <p>
+            Now you can shop online, select all what you need from the pharmacy through
+             our online store on web or on mobile app., and we will deliver it to you directly from our diffuse branches.
+        </p>
+        <a href="#" class="btn">Join Now</a>
+    </div>
+    <img src="images/doct.png.png" class="feature-img">
+ </div>
+</body>
+</html>
