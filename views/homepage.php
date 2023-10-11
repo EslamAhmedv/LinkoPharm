3f25354520e4ae2723333d5dae7a5a7e247b8e2d
Mohamedali1111 <118278730+Mohamedali1111@users.noreply.github.com>
2023-10-11 23:18:47 +0300

    create profile drop down menu

--- views/homepage.php
+++ views/homepage.php
@@ -4,7 +4,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link rel="stylesheet" href="../public/css/homepage.css">
-    
+    <link
+      rel="stylesheet"
+      href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css"
+    />
 </head>
 <body>
  <div class="hero">
@@ -15,11 +18,78 @@
             <li><a href="#">Cart</a></li>
             <li><a href="#">Profile</a></li>
         </ul>
+        
+    <div class="profile-dropdown">
+        <div onclick="toggle()" class="profile-dropdown-btn">
+          <div class="profile-img">
+            <i class="fa-solid fa-circle"></i>
+          </div>
+
+          <span>
+         user1
+            <i class="fa-solid fa-angle-down"></i>
+          </span>
+        </div>
+
+        <ul class="profile-dropdown-list">
+          <li class="profile-dropdown-list-item">
+            <a href="dashborad.php">
+              <i class="fa-regular fa-user"></i>
+              Edit Profile
+            </a>
+          </li>
+
+          <li class="profile-dropdown-list-item">
+            <a href="#">
+              <i class="fa-regular fa-envelope"></i>
+              Inbox
+            </a>
+          </li>
+
+          <li class="profile-dropdown-list-item">
+            <a href="#">
+              <i class="fa-solid fa-chart-line"></i>
+              Analytics
+            </a>
+          </li>
+
+          <li class="profile-dropdown-list-item">
+            <a href="#">
+              <i class="fa-solid fa-sliders"></i>
+              Settings
+            </a>
+          </li>
+
+          <li class="profile-dropdown-list-item">
+            <a href="#">
+              <i class="fa-regular fa-circle-question"></i>
+              Help & Support
+            </a>
+          </li>
+          <hr />
+
+          <li class="profile-dropdown-list-item">
+            <a href="#">
+              <i class="fa-solid fa-arrow-right-from-bracket"></i>
+              Log out
+            </a>
+          </li>
+        </ul>
+      </div>
         <div>
             <a href="#" class="login-btn">Log in</a>
             <a href="#" class="btn">Download app</a>
         </div>
-    </nav>
+   
+
+
+      </nav>
+
+
+
+
+
+
     <div class="content">
         <h1>Stay Home <br> & Order Online</h1>
         <p>
@@ -28,7 +98,8 @@
         </p>
         <a href="#" class="btn">Join Now</a>
     </div>
-    <img src="../public/images/doct.png.png" class="feature-img">
+    <!-- <img src="../public/images/doct.png.png" class="feature-img"> -->
  </div>
+
 </body>
 </html>
