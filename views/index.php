c05fe87ca7528363ece10b09794d28d350c6fd70
Mohamedali1111 <118278730+Mohamedali1111@users.noreply.github.com>
2023-10-13 05:25:13 +0300

    create chatbox

--- views/index.php
+++ views/index.php
@@ -3,12 +3,17 @@
 <head>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="../public/css/homepage.css">
+   
     <link
       rel="stylesheet"
       href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css"
     />
+    <link href='https://unpkg.com/boxicons@2.1.1/css/boxicons.min.css' rel='stylesheet'>
+    <link rel="stylesheet" href="../public/css/homepage.css">
+    <link rel="stylesheet" href="../public/css/chat.css">
 </head>
+
+
 <body>
  <div class="hero">
     <nav>
@@ -101,7 +106,51 @@
     <!-- <img src="../public/images/doct.png.png" class="feature-img"> -->
  </div>
 
- <script>let profileDropdownList = document.querySelector(".profile-dropdown-list");
+
+
+
+
+<div class="chatbox-wrapper">
+		<div class="chatbox-toggle">
+			<i class='bx bx-message-dots'></i>
+		</div>
+		<div class="chatbox-message-wrapper">
+			<div class="chatbox-message-header">
+				<div class="chatbox-message-profile">
+					<img src="../public/images/profile-pic.jpg" alt="" class="chatbox-message-image">
+					<div>
+						<h4 class="chatbox-message-name">user1</h4>
+						<p class="chatbox-message-status">online</p>
+					</div>
+				</div>\
+				<div class="chatbox-message-dropdown">
+					<i class='bx bx-dots-vertical-rounded chatbox-message-dropdown-toggle'></i>
+					<ul class="chatbox-message-dropdown-menu">
+						<li>
+							<a href="#">Search</a>
+						</li>
+						<li>
+							<a href="#">Report</a>
+						</li>
+					</ul>
+				</div>
+			</div>
+			<div class="chatbox-message-content">
+				<h4 class="chatbox-message-no-message">You don't have message yet!</h4>
+
+			</div>
+			<div class="chatbox-message-bottom">
+				<form action="#" class="chatbox-message-form">
+					<textarea rows="1" placeholder="Type message..." class="chatbox-message-input"></textarea>
+					<button type="submit" class="chatbox-message-submit"><i class='bx bx-send' ></i></button>
+				</form>
+			</div>
+		</div>
+	</div>
+	<script src="../public/js/chat.js"></script>
+
+
+  <script>let profileDropdownList = document.querySelector(".profile-dropdown-list");
 let btn = document.querySelector(".profile-dropdown-btn");
 
 let classList = profileDropdownList.classList;
