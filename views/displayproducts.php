4310ee40a0b2626007414cffd8495c059d987788
EslamAhmedv <125676587+EslamAhmedv@users.noreply.github.com>
2023-10-14 20:58:51 +0300

    Update displayproducts.php

--- views/displayproducts.php
+++ views/displayproducts.php
@@ -1,4 +1,11 @@
 
+<?php
+require '../includes/db.php';
+
+// Fetch products data from the database
+$sql = "SELECT * FROM products";
+$result = $conn->query($sql);
+?>
 
 <!DOCTYPE html>
 <html lang="en">
@@ -6,7 +13,6 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <!-- Material Icons CDN -->
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Sharp" />
     <link rel="stylesheet" href="../public/css/displayproducts.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
