b54afe7e5fdb471aa3f2dd71cf8490a42d2e6749
Mohamedali1111 <118278730+Mohamedali1111@users.noreply.github.com>
2023-10-14 20:14:25 +0300

    include chat into homepage

--- views/index.php
+++ views/index.php
@@ -10,7 +10,7 @@
     />
     <link href='https://unpkg.com/boxicons@2.1.1/css/boxicons.min.css' rel='stylesheet'>
     <link rel="stylesheet" href="../public/css/homepage.css">
-    <link rel="stylesheet" href="../public/css/chat.css">
+   
     <link rel="stylesheet" href="../public/css/categories.css">
     <link rel="stylesheet" href="../public/css/footer.css">
 </head>
@@ -127,34 +127,7 @@
   <span class="dot" onclick="currentSlide(3)"></span> 
 </div>
 
-<script>
-let slideIndex = 1;
-showSlides(slideIndex);
 
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
-
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-  let i;
-  let slides = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}    
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";  
-  }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";  
-  dots[slideIndex-1].className += " active";
-}
-</script>
       </div>
 
       </nav>
@@ -174,17 +147,6 @@ function showSlides(n) {
 
 
 
-  <script>let profileDropdownList = document.querySelector(".profile-dropdown-list");
-let btn = document.querySelector(".profile-dropdown-btn");
-
-let classList = profileDropdownList.classList;
-
-const toggle = () => classList.toggle("active");
-
-window.addEventListener("click", function (e) {
-  if (!btn.contains(e.target)) classList.remove("active");
-});
-</script>
 
 <section class="section__container sale__container">
     <h2 class="section__title">OUR CATEGORIES </h2>
@@ -297,43 +259,68 @@ window.addEventListener("click", function (e) {
   	 </div>
   </footer>
 
-  <div class="chatbox-wrapper">
-		<div class="chatbox-toggle">
-			<i class='bx bx-message-dots'></i>
-		</div>
-		<div class="chatbox-message-wrapper">
-			<div class="chatbox-message-header">
-				<div class="chatbox-message-profile">
-					<img src="../public/images/profile-pic.jpg" alt="" class="chatbox-message-image">
-					<div>
-						<h4 class="chatbox-message-name">user1</h4>
-						<p class="chatbox-message-status">online</p>
-					</div>
-				</div>
-				<div class="chatbox-message-dropdown">
-					<i class='bx bx-dots-vertical-rounded chatbox-message-dropdown-toggle'></i>
-					<ul class="chatbox-message-dropdown-menu">
-						<li>
-							<a href="#">Search</a>
-						</li>
-						<li>
-							<a href="#">Report</a>
-						</li>
-					</ul>
-				</div>
-			</div>
-			<div class="chatbox-message-content">
-				<h4 class="chatbox-message-no-message">You don't have message yet!</h4>
 
-			</div>
-			<div class="chatbox-message-bottom">
-				<form action="#" class="chatbox-message-form">
-					<textarea rows="1" placeholder="Type message..." class="chatbox-message-input"></textarea>
-					<button type="submit" class="chatbox-message-submit"><i class='bx bx-send' ></i></button>
-				</form>
-			</div>
-		</div>
-	</div>
-	<script src="../public/js/chat.js"></script>
+
+  <?php include 'chat.php';?>
+
+
+  <script>
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+}
+</script>
+
+
+
+
+
+
+<script>let profileDropdownList = document.querySelector(".profile-dropdown-list");
+let btn = document.querySelector(".profile-dropdown-btn");
+
+let classList = profileDropdownList.classList;
+
+const toggle = () => classList.toggle("active");
+
+window.addEventListener("click", function (e) {
+  if (!btn.contains(e.target)) classList.remove("active");
+});
+</script>
+
+
+
+
+
+
+
+
+
+
+
+
+
 </body>
 </html>
