3ef941bca6771a4ac21f2b2907b894caab7573e2
Mayawaleed <125691425+Mayawaleed@users.noreply.github.com>
2023-10-15 18:16:56 +0300

    404 error page php

---
+++ views/404page.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <link rel="stylesheet" href="css/404page.css">
+        <title>404 error!</title>
+         <link href="https://fonts.googleapis.com/css2?family=Jost:wght@500&display=swap" rel="stylesheet">
+         <link rel="stylesheet" href="../public/css/forgetpass.css">
+        <link  href="https://fontawesome.com/icons/circle-exclamation?f=classic&s=regular" rel ="stylesheet"> 
+</head>
+        <body>
+        <br>
+<br>
+        <div class="form">
+        <i class="fa-regular fa-circle-exclamation"></i>
+
+        <label>
+
+            <h1>OPPS!</h1>
+
+            <h3>404-PAGE NOT FOUND</h3>
+
+            <P><b>something went wrong.. try again later</b></p>
+
+            <p>The page you are looking for might have been removed</P>
+            <P> or had its name changed or is temporarily unavailable</p>
+</label>
+
+            <button type="button" class="submit">GO TO HOME PAGE</button>
+</div>
