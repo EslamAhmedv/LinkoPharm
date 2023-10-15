3668a63d8d555cb282e5ad940aea2eed1db389a7
Mayawaleed <125691425+Mayawaleed@users.noreply.github.com>
2023-10-15 16:55:32 +0300

    forgetpass2 php

---
+++ views/forgetpass2.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <link rel="stylesheet" href="css/forgetpass2.css">
+
+        <title>forget pass</title>
+
+         <link href="https://fonts.googleapis.com/css2?family=Jost:wght@500&display=swap" rel="stylesheet">
+         <link rel="stylesheet" href="../public/css/forgetpass.css">
+         <link rel="stylesheet" href="https://unicons.iconscout.com/release/v4.0.0/css/line.css"> 
+</head>
+
+        <body>
+        <br>
+<br>
+    <div class="cont">
+        <div class="form">
+            <span class="txtpass">  New Password </span>
+            <i class="uil uil-lock"></i>
+            <br>
+            <br>
+            Please create a new password 
+            <br>
+            that you don't use on any other site..
+
+            <label>
+            
+                
+                <br>
+                <input type="password" placeholder="Password" required>
+            
+                <i class="uil uil-eye-slash"></i>
+            </label>
+
+            <label>
+            
+                <br>
+                <input type="Cpassword" placeholder="Confirm Password" required>
+           
+                <i class="uil uil-eye-slash"></i>
+            </label>
+
+            <button type="button" class="submit">Change</button>
