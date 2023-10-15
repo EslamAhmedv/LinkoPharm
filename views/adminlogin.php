984c6d19721315190d8a3e7e21a78746c8b59f31
Mayawaleed <125691425+Mayawaleed@users.noreply.github.com>
2023-10-15 20:13:21 +0300

    adminlogin php

---
+++ views/adminlogin.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <link rel="stylesheet" href="css/adminlogin.css">
+        <title>Admin login</title>
+        <link rel="stylesheet" href="https://unicons.iconscout.com/release/v4.0.0/css/line.css"> 
+         <link href="https://fonts.googleapis.com/css2?family=Jost:wght@500&display=swap" rel="stylesheet">
+         <link rel="stylesheet" href="../public/css/adminlogin.css">
+</head>
+        <body>
+        <br>
+<br>
+    <div class="cont">
+        <div class="form Log-in">
+            <h1>Admin Login</h1>
+            <label>
+            <i class="uil uil-envelope"></i>
+                <span>Email :</span>
+                <input type="email" />
+            </label>
+            <label>
+                <br>
+            <i class="uil uil-lock"></i>
+                <span>Password :</span>
+                <input type="password" />
+            </label>
+            <button type="button" class="submit">Sign In</button>
