17d3f26b6927dfaaf877523719d40e25cadde8b3
Mayawaleed <125691425+Mayawaleed@users.noreply.github.com>
2023-10-24 01:40:44 +0300

    edit prodlist

--- views/productslist.php
+++ views/productslist.php
@@ -22,7 +22,7 @@
                     <div class="design">
                         <span>Skin prod</span>
                         <h5>Sunscreen</h5>
-                      <del>$850</del>$800
+                        <del>$345<span></del>$275
                       <button class="btn"><i class="uil uil-shopping-cart-alt"></i></button>
                       <button class="btn"><i class="uil uil-heart"></i></button>
                     </div>
@@ -33,18 +33,18 @@
                     <div class="design">
                         <span>Hair product</span>
                         <h5>Hair vitamin</h5>
-                        <del>$800</del>$680
+                        <del>$345<span></del>$275
                         <button class="btn"><i class="uil uil-shopping-cart-alt"></i></button>
                       <button class="btn"><i class="uil uil-heart"></i></button>
                     </div>
                 </div>
                 <div class="prod">
-                    <img src="../public/images/prod1-removebg-preview.png" alt="bed3">
+                    <img src="../public/images/prod1-removebg-preview.png" alt="pic3">
 
                     <div class="design">
                         <span>Hair product</span>
                         <h5>Hair vitamin </h5>
-                        <del>$2000</del>$1500
+                        <del>$345<span></del>$275
                         <button class="btn"><i class="uil uil-shopping-cart-alt"></i></button>
                       <button class="btn"><i class="uil uil-heart"></i></button>
                     </div>
@@ -52,12 +52,12 @@
 
 
                 <div class="prod">
-                    <img src="../public/images/prod4-removebg-preview.png " alt="bed4">
+                    <img src="../public/images/prod4-removebg-preview.png " alt="pic4">
 
                     <div class="design">
                         <span>Hair-Skin product</span>
                         <h5>Hair and skin vitamin</h5>
-                        <del>$2800</del>$2000
+                        <del>$345<span></del>$275
                         <button class="btn"><i class="uil uil-shopping-cart-alt"></i></button>
                       <button class="btn"><i class="uil uil-heart"></i></button>
             </div>
@@ -68,7 +68,7 @@
                 <tr>
                 <td>
                   <div class="prod">
-                  <img src="../public/images/prod5-removebg-preview.png" alt="bed5">
+                  <img src="../public/images/prod5-removebg-preview.png" alt="pic5">
                   <div class="design">
                     <span>Hair-Skin product</span>
                     <h5>Hair and skin vitamin</h5>
@@ -81,12 +81,12 @@
 
                      <td>
                      <div class="prod">
-                        <img src="../public/images/prod7-removebg-preview.png " alt="bed6">
+                        <img src="../public/images/prod7-removebg-preview.png " alt="pic6">
                         <div class="design">
                           <span>Skin product</span>
                           <h5>Shampoo</h5>
                          
-                          <div class=btn> <del>$345<span></del>$275</div>
+                           <del>$345<span></del>$275
                          
                           <button class="btn"><i class="uil uil-shopping-cart-alt"></i></button>
                       <button class="btn"><i class="uil uil-heart"></i></button>
@@ -96,11 +96,72 @@
 
                     <td>
                      <div class="prod">
-                    <img src="../public/images/prod10-removebg-preview.png" alt="bed7">
+                    <img src="../public/images/prod10-removebg-preview.png" alt="pic7">
                     <div class="design">
                       <span>skin products</span>
                       <h5>Deodrant</h5>
                       <del>$155<span></del>$145
+                      <button class="btn"><i class="uil uil-shopping-cart-alt"></i></button>
+                      <button class="btn"><i class="uil uil-heart"></i></button>
+                   </div>
+                   </div> 
+                   </td>
+                   <td>
+                     <div class="prod">
+                    <img src="../public/images/wish1.webp" alt="pic8">
+                    <div class="design">
+                      <span>skin products</span>
+                      <h5>Makeup remover</h5>
+                      <del>$800<span></del>$750
+                      <button class="btn"><i class="uil uil-shopping-cart-alt"></i></button>
+                      <button class="btn"><i class="uil uil-heart"></i></button>
+                   </div>
+                   </div> 
+                   </td>
+                   <tr>
+                   <td>
+                     <div class="prod">
+                    <img src="../public/images/prod9.png" alt="pic8">
+                    <div class="design">
+                      <span>Skin products</span>
+                      <h5>Body cream</h5>
+                      <del>$800<span></del>$750
+                      <button class="btn"><i class="uil uil-shopping-cart-alt"></i></button>
+                      <button class="btn"><i class="uil uil-heart"></i></button>
+                   </div>
+                   </div> 
+                   </td>
+                   <td>
+                     <div class="prod">
+                    <img src="../public/images/prod6.png" alt="pic8">
+                    <div class="design">
+                      <span>Skin products</span>
+                      <h5>Body gel</h5>
+                      <del>$120<span></del>$100
+                      <button class="btn"><i class="uil uil-shopping-cart-alt"></i></button>
+                      <button class="btn"><i class="uil uil-heart"></i></button>
+                   </div>
+                   </div> 
+                   </td>
+                   <td>
+                     <div class="prod">
+                    <img src="../public/images/prod12.png" alt="pic8">
+                    <div class="design">
+                      <span>Tablets</span>
+                      <h5>Iron and vitamin </h5>
+                      <del>$1200<span></del>$1100
+                      <button class="btn"><i class="uil uil-shopping-cart-alt"></i></button>
+                      <button class="btn"><i class="uil uil-heart"></i></button>
+                   </div>
+                   </div> 
+                   </td>
+                   <td>
+                     <div class="prod">
+                    <img src="../public/images/prod8.png" alt="pic8">
+                    <div class="design">
+                      <span>Skin products</span>
+                      <h5>Gel cleanser </h5>
+                      <del>$570<span></del>$550
                       <button class="btn"><i class="uil uil-shopping-cart-alt"></i></button>
                       <button class="btn"><i class="uil uil-heart"></i></button>
                    </div>
