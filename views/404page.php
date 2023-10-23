8d0ba371c1cc29c9ee5c1ed5881c2b646bbbf0b1
Mayawaleed <125691425+Mayawaleed@users.noreply.github.com>
2023-10-23 19:20:24 +0300

    404 edit php

--- views/404page.php
+++ views/404page.php
@@ -1,29 +1,33 @@
-<!DOCTYPE html>
-<html>
+<!doctype html>
+<html lang="en">
+
     <head>
-        <link rel="stylesheet" href="css/404page.css">
-        <title>404 error!</title>
-         <link href="https://fonts.googleapis.com/css2?family=Jost:wght@500&display=swap" rel="stylesheet">
-         <link rel="stylesheet" href="../public/css/forgetpass.css">
-        <link  href="https://fontawesome.com/icons/circle-exclamation?f=classic&s=regular" rel ="stylesheet"> 
+        <meta charset="utf-8">   
+<meta name= "viewport" content="intial-scale=1.0,maximum-scale=1.0
+,user-scalable=1">
+<title>
+    404 error
+</title>
+<link rel="stylesheet" type="text/css" href="css/404 error.css">
 </head>
-        <body>
-        <br>
-<br>
-        <div class="form">
-        <i class="fa-regular fa-circle-exclamation"></i>
 
-        <label>
-
-            <h1>OPPS!</h1>
-
-            <h3>404-PAGE NOT FOUND</h3>
-
-            <P><b>something went wrong.. try again later</b></p>
+<body>
+    <div class="container_404">
+        <div class="left_404">
+            <img src="../public/images/sadface.png">
+            
+</div>
+<div class="right_404">
+    <h3>OH NO!<span> 404</span></h3>
+    <h1>Page Not Found</h1>
+    <P><b>something went wrong.. try again later</b></p>
 
             <p>The page you are looking for might have been removed</P>
             <P> or had its name changed or is temporarily unavailable</p>
-</label>
 
-            <button type="button" class="submit">GO TO HOME PAGE</button>
+            <a href="#" class="btn_404">Go back to home</a>
+
 </div>
+
+</body>
+</html>
