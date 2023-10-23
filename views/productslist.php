9763d1f6febfffbb9681446a384696d66d7beb21
Mayawaleed <125691425+Mayawaleed@users.noreply.github.com>
2023-10-23 22:23:44 +0300

    prodlist php

---
+++ views/productslist.php
@@ -0,0 +1,94 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Linkopharm Products</title>
+    <link rel="stylesheet" href="https://unpkg.com/swiper/swiper-bundle.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fontawesome/5.15.3/css/all.min.css">
+    
+    <script src="https://kit.fontawesome.com/f4bd0b4361.js" crossorigin="anonymous"></script>
+</head>
+<body>
+
+<section class="section__container sale__container">
+    <h2 class="section__title">Our Products </h2>
+    <div class="sale__grid">
+      <div class="sale__card">
+        <img src="../public/images/skin care.jpg" alt="sale" />
+        <div class="sale__content">
+          <p class="sale__subtitle">SKIN CARE</p>
+          <h4 class="sale__title">sale <span> </span>off </h4>
+          <p class="sale__subtitle">Dont miss</p>
+          <button class="btnn sale__btnn">shop now </button>
+
+        </div>
+      </div>
+
+
+      <div class="sale__card">
+        <img src="../public/images/haircare.jpg" alt="sale" />
+        <div class="sale__content">
+          <p class="sale__subtitle">
+            HAIR CARE</p>
+          <h4 class="sale__title">sale <span>40% </span>off </h4>
+          <p class="sale__subtitle">Dont miss</p>
+          <button class="btnn sale__btnn">shop now </button>
+
+        </div>
+      </div>
+
+
+      <div class="sale__card">
+      <img src="../public/images/oral.jpg" alt="sale" />
+      <div class="sale__content">
+        <p class="sale__subtitle">ORAL CARE</p>
+        <h4 class="sale__title">sale <span>40% </span>off </h4>
+        <p class="sale__subtitle">Dont miss</p>
+        <button class="btnn sale__btnn">shop now </button>
+
+      </div>
+    </div>
+
+      <div class="sale__card">
+        <img src="../public/images/nutrients.jpg" alt="sale" />
+        <div class="sale__content">
+          <p class="sale__subtitle">Nurients </p>
+          <h4 class="sale__title">sale <span>40% </span>off </h4>
+          <p class="sale__subtitle">Dont miss</p>
+          <button class="btnn sale__btnn">shop now </button>
+
+        </div>
+      </div>
+
+
+
+
+      <div class="sale__card">
+        <img src="../public/images/TOPICAL MUSCLE RELAXANTS.jpeg" alt="sale" />
+        <div class="sale__content">
+          <p class="sale__subtitle">TOPICAL MUSCLE RELAXANTS</p>
+          <h4 class="sale__title">sale <span>40% </span>off </h4>
+          <p class="sale__subtitle">Dont miss</p>
+          <button class="btnn sale__btnn">shop now </button>
+
+        </div>
+      </div>
+
+
+      <div class="sale__card">
+        <img src="../public/images/ff.jpg" alt="sale" />
+        <div class="sale__content">
+          <p class="sale__subtitle">TOPICAL MUSCLE RELAXANTS</p>
+          <h4 class="sale__title">sale <span>40% </span>off </h4>
+          <p class="sale__subtitle">Dont miss</p>
+          <button class="btnn sale__btnn">shop now </button>
+
+        </div>
+      </div>
+
+
+
+    </div>
+  </section>
+
