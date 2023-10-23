6d310640fa74cc1b687500c3bb0dfcb922059863
Mohamedali1111 <118278730+Mohamedali1111@users.noreply.github.com>
2023-10-23 21:30:28 +0300

    change register form

--- views/login.php
+++ views/login.php
@@ -1,187 +1,64 @@
-<?php
-// @include '../includes/db.php';
-// if(isset($_POST['submit'])){
-//     $username= mysqli_real_escape_string($conn,$_POST['username']);
-//     $email= mysqli_real_escape_string($conn,$_POST['email']);
-//     $password= md5($_POST['password']);
-//     $cpassword= md5($_POST['password2']);
-
-//     $select = " SELECT * FROM users WHERE email = '$email' && password = '$password' ";
-//     $result = mysqli_query($conn,$select);
-//     if(mysqli_num_rows($result)>0){
-//         $error[]='user already exist';
-//     }else{
-//         if($password != $cpassword){
-//             $error[]='password doesn\'t match  exist';
-
-//         }else{
-//             $insert="INSERT INTO users(username,email,password) VALUES('$username','$email','$password')";
-//             mysqli_query($conn,$insert);
-//             header('location:index.php');
-//         }
-//     }
-
-// };
-
-
-
-require "functions.php";
-
-$errors = array();
-
-if($_SERVER['REQUEST_METHOD'] == "POST")
-{
-
-	$errors = signup($_POST);
-
-	if(count($errors) == 0)
-	{
-		header("Location: index.php");
-		die;
-	}
-}
-
-
-$errors2 = array();
-
-if($_SERVER['REQUEST_METHOD'] == "POST")
-{
-
-	$errors2 = login($_POST);
-
-	if(count($errors2) == 0)
-	{
-		header("Location: dashboard.php");
-		die;
-	}
-}
-
-?>
-
-
-<?php
-
-
-?>
-
-
-
-
-
 <!DOCTYPE html>
 <html>
-    <head>
-        <link rel="stylesheet" href="css/slide.css">
-        <title>login and sign up</title>
-         <link href="https://fonts.googleapis.com/css2?family=Jost:wght@500&display=swap" rel="stylesheet">
-         <link rel="stylesheet" href="../public/css/login.css">
-</head>
-        <body>
-        <br>
-<br>
-    <div class="cont">
-        <div class="form Log-in">
-            <h2>Welcome</h2>
-            <form action="" method="post">
-            <?php if(count($errors2) > 0):?>
-				<?php foreach ($errors2 as $error2):?>
-					<?= $error2?> <br>	
-				<?php endforeach;?>
-			<?php endif;?>
-            <label>
-                <span>Email</span>
-                <input type="email" name="email" />
-            </label>
-            <label>
-                <span>Password</span>
-                <input type="password" name="password"/>
-            </label>
-            <p class="forgot-pass">Forgot password?</p>
-            <label class="container">Admin:
-          <input type="checkbox" checked="checked">
-          <span class="checkmark"></span>
-          </label>
+	<head>
+		<meta charset="utf-8">
+		<title>RegistrationForm_v1 by Colorlib</title>
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		
+		<!-- MATERIAL DESIGN ICONIC FONT -->
+		<link rel="stylesheet" href="fonts/material-design-iconic-font/css/material-design-iconic-font.min.css">
 
-          <label class="container">User:
-          <input type="checkbox">
-          <span class="checkmark"></span>
-          </label>
-            <button type="submit" class="submit">Sign In</button>
-             </form>
-         
-        </div>
-        <div class="sub-cont">
-            <div class="img">
-                <div class="img__text m--up">
-                 
-                    <h3>Don't have an account? Please Sign up!<h3>
-                </div>
-                <div class="img__text m--in">
-                
-                    <h3>If you already has an account, just sign in.<h3>
-                </div>
-                <div class="img__btn">
-                    <span class="m--up">Sign Up</span>
-                    <span class="m--in">Sign In</span>
-                </div>
-            </div>
-            <div class="form sign-up">
-                <h2>Create your Account</h2>
-                <form action="" method="post">
-                    <?php 
-                    // if(isset($error)){
-                    //     foreach($error as $error){
-                    //         echo '<span class="error-msg">'.$error.'</span>';
-                    //     };
-                    // };
-                    ?>
+		<!-- STYLE CSS -->
+		<link rel="stylesheet" href="../public/css/login.css">
+	</head>
 
-<?php if(count($errors) > 0):?>
-				<?php foreach ($errors as $error):?>
-					<?= $error?> <br>	
-				<?php endforeach;?>
-			<?php endif;?>
-            <label>
-                <span>First Name</span>
-                <input type="fname" />
-            </label>
-            <label>
-                <span>Last Name</span>
-                <input type="lname" />
-            </label>
-            <label>
-                <span>Phone</span>
-                <input type="phone" />
-            </label>
-            <label>
-                <span>Address</span>
-                <input type="address" />
-            </label>
-            <label>
-                <span>Age</span>
-                <input type="age" />
-            </label>
-                <label>
-                    <span>Email</span>
-                    <input type="text" Name="email" />
-                </label>
-                <label>
-                    <span>Password</span>
-                    <input type="text" Name="password" />
-                </label>
+	<body>
+    <?php
 
-                
+include('../partials/navbar.php'); ?>
 
-                <button type="submit" name="submit" class="submit">Sign Up</button>
-                </form>
-            </div>
-        </div>
-    </div>
-
-    <script>
-        document.querySelector('.img__btn').addEventListener('click', function() {
-            document.querySelector('.cont').classList.toggle('s--signup');
-        });
-    </script>
-</body>
+		<div class="wrapper" style="background-image: url('images/bg-registration-form-1.jpg');">
+			<div class="inner">
+				<div class="image-holder">
+					<img src="../public/images/doct.png.png" alt="">
+				</div>
+				<form action="">
+					<h3>Registration Form</h3>
+					<div class="form-group">
+						<input type="text" placeholder="First Name" class="form-control">
+						<input type="text" placeholder="Last Name" class="form-control">
+					</div>
+					<div class="form-wrapper">
+						<input type="text" placeholder="Username" class="form-control">
+						<i class="zmdi zmdi-account"></i>
+					</div>
+					<div class="form-wrapper">
+						<input type="text" placeholder="Email Address" class="form-control">
+						<i class="zmdi zmdi-email"></i>
+					</div>
+					<div class="form-wrapper">
+						<select name="" id="" class="form-control">
+							<option value="" disabled selected>Gender</option>
+							<option value="male">Male</option>
+							<option value="femal">Female</option>
+							<option value="other">Other</option>
+						</select>
+						<i class="zmdi zmdi-caret-down" style="font-size: 17px"></i>
+					</div>
+					<div class="form-wrapper">
+						<input type="password" placeholder="Password" class="form-control">
+						<i class="zmdi zmdi-lock"></i>
+					</div>
+					<div class="form-wrapper">
+						<input type="password" placeholder="Confirm Password" class="form-control">
+						<i class="zmdi zmdi-lock"></i>
+					</div>
+					<button>Register
+						<i class="zmdi zmdi-arrow-right"></i>
+					</button>
+				</form>
+			</div>
+		</div>
+		
+	</body><!-- This templates was made by Colorlib (https://colorlib.com) -->
 </html>
