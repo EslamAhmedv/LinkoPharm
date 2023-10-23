aad179f507ca13c4f9bce7dc319d53086804774c
youssef <youssef1352019@icloud.com>
2023-10-23 07:32:37 +0300

    Edit css

--- views/prodDetails.php
+++ views/prodDetails.php
@@ -25,10 +25,6 @@ $product = [
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../public/css/prodDetails.css">
-    <link rel="stylesheet" href="../public/css/homepage.css">
-    <link
-      rel="stylesheet"
-      href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css"/>
     <title>Product details - <?php echo $product['name']; ?></title>
 </style>
 </head>
