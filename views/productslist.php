99fbc30dc66a2bcb7a621575a95c9f65c2610666
Mayawaleed <125691425+Mayawaleed@users.noreply.github.com>
2023-10-24 16:10:44 +0300

    responsive prodlist

--- views/productslist.php
+++ views/productslist.php
@@ -5,7 +5,7 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Product Display</title>
-  <link rel="stylesheet" href="css/productlist.css">
+  <link rel="stylesheet" href="../public/css/productslist.css">
 </head>
 
 <body>
@@ -13,86 +13,86 @@
   <div class="product-container">
     <div class="card">
       <img src="../public/images/prod5-removebg-preview.png" alt="Denim Jeans" style="width:100%">
-      <h1>Tailored Jeans</h1>
-      <p class="price">$19.99</p>
-      <p>Some text about the jeans..</p>
+      <h1>Hair gel</h1>
+      <p>Hair product</p>
+      <p class="price">$200.99</p>
       <p><button>Add to Cart</button></p>
     </div>
     <div class="card">
       <img src="../public/images/wish2.webp" alt="Denim Jeans" style="width:100%">
-      <h1>Tailored Jeans</h1>
-      <p class="price">$19.99</p>
-      <p>Some text about the jeans..</p>
+      <h1>Sun screen</h1>
+     <p>Skin product</p>
+       <p class="price">$600</p>
       <p><button>Add to Cart</button></p>
     </div>
     <div class="card">
       <img src="../public/images/prod3-removebg-preview.png" alt="Denim Jeans" style="width:100%">
-      <h1>Tailored Jeans</h1>
-      <p class="price">$19.99</p>
-      <p>Some text about the jeans..</p>
+      <h1>Hair cream</h1>
+      <p>Hair product</p>
+      <p class="price">$360</p>
       <p><button>Add to Cart</button></p>
     </div>
     <div class="card">
       <img src="../public/images/prod1-removebg-preview.png" alt="Denim Jeans" style="width:100%">
-      <h1>Tailored Jeans</h1>
-      <p class="price">$19.99</p>
-      <p>Some text about the jeans..</p>
+      <h1>vitamin</h1>
+      <p>Hair-skin product</p>
+      <p class="price">$1200</p>
       <p><button>Add to Cart</button></p>
     </div>
     <div class="card">
       <img src="../public/images/prod4-removebg-preview.png" alt="Denim Jeans" style="width:100%">
-      <h1>Tailored Jeans</h1>
-      <p class="price">$19.99</p>
-      <p>Some text about the jeans..</p>
+      <h1>vitamin</h1>
+      <p>Hair-skin product</p>
+      <p class="price">$1600</p>
       <p><button>Add to Cart</button></p>
     </div>
     <div class="card">
-      <img src="../public/images/prod5-removebg-preview.png" alt="Denim Jeans" style="width:100%">
-      <h1>Tailored Jeans</h1>
-      <p class="price">$19.99</p>
-      <p>Some text about the jeans..</p>
+      <img src="../public/images/prod8.png" alt="Denim Jeans" style="width:100%">
+      <h1>Hair spray</h1>
+      <p>Hair product</p>
+       <p class="price">$500</p>
       <p><button>Add to Cart</button></p>
     </div>
     <div class="card">
       <img src="../public/images/prod7-removebg-preview.png" alt="Denim Jeans" style="width:100%">
-      <h1>Tailored Jeans</h1>
-      <p class="price">$19.99</p>
-      <p>Some text about the jeans..</p>
+      <h1>Body mist</h1>
+      <p class="price">$340</p>
+      <p>Skin product</p>
       <p><button>Add to Cart</button></p>
     </div>
     <div class="card">
-      <img src="../public/images/prod13.png" alt="Denim Jeans" style="width:100%">
-      <h1>Tailored Jeans</h1>
-      <p class="price">$19.99</p>
-      <p>Some text about the jeans..</p>
+      <img src="../public/images/prod2.png" alt="Denim Jeans" style="width:100%">
+    <h1>Hair cream</h1>
+      <p>Hair product</p>
+      <p class="price">$360</p>
       <p><button>Add to Cart</button></p>
     </div>
     <div class="card">
       <img src="../public/images/prod10.png" alt="Denim Jeans" style="width:100%">
-      <h1>Tailored Jeans</h1>
-      <p class="price">$19.99</p>
-      <p>Some text about the jeans..</p>
+      <h1>Deodrant</h1>
+      <p class="price">$240</p>
+      <p>Skin product</p>
       <p><button>Add to Cart</button></p>
     </div>
     <div class="card">
       <img src="../public/images/prod9.png" alt="Denim Jeans" style="width:100%">
-      <h1>Tailored Jeans</h1>
-      <p class="price">$19.99</p>
-      <p>Some text about the jeans..</p>
+      <h1>Hair cream</h1>
+      <p>Hair product</p>
+      <p class="price">$460</p>
       <p><button>Add to Cart</button></p>
     </div>
     <div class="card">
       <img src="../public/images/prod6.png" alt="Denim Jeans" style="width:100%">
-      <h1>Tailored Jeans</h1>
-      <p class="price">$19.99</p>
-      <p>Some text about the jeans..</p>
+      <h1>Hair cream</h1>
+      <p>Hair product</p>
+      <p class="price">$860</p>
       <p><button>Add to Cart</button></p>
     </div>
     <div class="card">
       <img src="../public/images/prod12.png" alt="Denim Jeans" style="width:100%">
-      <h1>Tailored Jeans</h1>
-      <p class="price">$19.99</p>
-      <p>Some text about the jeans..</p>
+      <h1>vitamin</h1>
+      <p>Hair-skin product</p>
+      <p class="price">$2200</p>
       <p><button>Add to Cart</button></p>
     </div>
   </div>
