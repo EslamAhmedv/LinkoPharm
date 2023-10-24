150b2174456a8155da1c0a1e1917c465bb9bf57b
nadaelamir101 <125657577+nadaelamir101@users.noreply.github.com>
2023-10-24 19:12:02 +0200

    aboutus.php

---
+++ views/aboutus.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<html>
+    <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../public/css/aboutus.css">
+    <title>Checkout</title>
+</style>
+</head>
+<body>
+    <div class="responsive-container-block bigContainer">
+  <div class="responsive-container-block Container bottomContainer">
+    <div class="ultimateImg">
+      <img class="mainImg"  src="../public/images/aboutus.jpg.jpg" alt="" />
+      <div class="purpleBox">
+        <p class="purpleText">
+        Linko-Pharm is a performance-oriented company where our employees are the company’s strongest talent. Their commitment, dedication and aspiration help create a fast moving
+        </p>
+        <img class="stars" src="https://workik-widget-assets.s3.amazonaws.com/widget-assets/images/mp5.svg">
+      </div>
+    </div>
+    <div class="allText bottomText">
+      <p class="text-blk headingText">
+        About Us
+      </p>
+      <p class="text-blk subHeadingText">
+      Linko Pharmaceuticals is a limited liability cosmoceutical company that was established in 2002 
+      </p>
+      <p class="text-blk description">
+      Linko Pharmaceuticals is a limited liability cosmoceutical company that was established in 2002 with its headquarters located in Cairo with the objective of providing high quality and innovative products to the Egyptian market.In 2011, after 9 years of operations, Linko Pharm has grown from a small local company to a market leader of approximately 500 employees.Our factory was established in year 2010 in Badr City. The plant has been constructed according to the broad outlines and principles of the GMP and GLP. We are a company that offers a diversified product range focused on cosmeceuticals and herbal drugs.
+      </p>
+      <a class="explore">
+        Contact Us
+      </a>
+    </div>
+  </div>
+</div>
+</body>
+</html>
