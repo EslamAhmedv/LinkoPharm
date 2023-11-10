8bc39c9b168747bbb71a315195dbe8498e05a6a6
Youssef <youssef1352019@icloud.com>
2023-11-10 03:19:39 +0200

    .

--- views/checkout.php
+++ views/checkout.php
@@ -177,16 +177,5 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             </div>
         </div>
     </div>
-    
-    <script>
-    let profileDropdownList = document.querySelector(".profile-dropdown-list");
-    let btn = document.querySelector(".profile-dropdown-btn"); 
-    let classList = profileDropdownList.classList;
-    const toggle = () => classList.toggle("active");
-    window.addEventListener("click", function (e) {
-        if (!btn.contains(e.target)) classList.remove("active");
-        });
-    </script>
-
-    </body>
+</body>
 </html>
