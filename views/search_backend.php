bc9a9a95f6fae1c556d1cfb741dbb4e93df935cd
nadaelamir101 <125657577+nadaelamir101@users.noreply.github.com>
2023-12-07 17:48:55 +0200

    fix search

--- views/search_backend.php
+++ views/search_backend.php
@@ -1,32 +1,23 @@
 <?php
-// Connect to your database
-// Assuming $conn is your database connection
-require_once("../includes/Dbh.php");
-  
+require_once "../models/ProductsModel.php"; 
+
+$productsModel = new ProductsModel();
+
 if (isset($_GET['search'])) {
     $searchTerm = $_GET['search'];
-global $conn;
-    // Perform a database query based on the search term
-    $query = "SELECT * FROM products WHERE ID LIKE '%$searchTerm%'";
-    $result = mysqli_query($conn, $query);
 
-    if ($result) {
-        if (mysqli_num_rows($result) > 0) {
-            while ($row = mysqli_fetch_assoc($result)) {
-                // Display search results (You can modify this according to your HTML structure)
-                echo '<p>' . $row['productName'] . '</p>';
-            }
-        } else {
-            echo '<p>No results found</p>';
+    $products = $productsModel->searchProducts($searchTerm);
+
+    if (!empty($products)) {
+        foreach ($products as $product) {
+            echo "Product Name: " . $product['name'] . "<br>";
+            echo "Description: " . $product['description'] . "<br>";
+            echo "<hr>";
         }
     } else {
-        echo '<p>Error executing the search</p>';
+        echo "No products found matching your search.";
     }
 } else {
-    echo '<p>No search term provided</p>';
+    echo "Please enter a search term.";
 }
-
-
-
 ?>
-
