40d15b4560e909a4f135181e5dd0be18b5fac838
Mayawaleed <125691425+Mayawaleed@users.noreply.github.com>
2023-12-09 01:08:42 +0200

    adding functions

--- models/medicalModel.php
+++ models/medicalModel.php
@@ -1,23 +1,45 @@
 <?php
-
-class MedicalModel
+class medicalModel
 {
+    private static $pdo;
+
+    // Database connection
+    private static function connect()
+    {
+        if (!isset(self::$pdo)) {
+            try {
+                self::$pdo = new PDO("mysql:host=localhost;dbname=your_database", "root", ""); // Assuming the default XAMPP credentials
+                self::$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            } catch (PDOException $e) {
+                die("Connection failed: " . $e->getMessage());
+            }
+        }
+    }
+
     public static function filterItems($category, $price)
     {
-        // Replace this with your actual database connection code
-        $pdo = new PDO("mysql:host=localhost;dbname=your_database", "your_username", "your_password");
+        self::connect();
 
         $sql = "SELECT * FROM medical_items WHERE 1";
 
         if ($category != 'all') {
-            $sql .= " AND category = '$category'";
+            $sql .= " AND category = :category";
         }
 
         if (!empty($price)) {
-            $sql .= " AND price <= $price";
+            $sql .= " AND price <= :price";
         }
 
-        $stmt = $pdo->prepare($sql);
+        $stmt = self::$pdo->prepare($sql);
+
+        if ($category != 'all') {
+            $stmt->bindParam(':category', $category);
+        }
+
+        if (!empty($price)) {
+            $stmt->bindParam(':price', $price);
+        }
+
         $stmt->execute();
 
         return $stmt->fetchAll(PDO::FETCH_ASSOC);
