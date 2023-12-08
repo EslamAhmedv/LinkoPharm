3c26181fd17c1b76141c58621894b1726d4fc257
Mayawaleed <125691425+Mayawaleed@users.noreply.github.com>
2023-12-08 22:27:41 +0200

    medicalConttroller

---
+++ controllers/medicalController.php
@@ -0,0 +1,24 @@
+<?php
+
+class MedicalController
+{
+    public function index()
+    {
+        require_once 'views/index.php';
+    }
+
+    public function filter()
+    {
+        require_once '../Models/MedicalModel.php';
+
+        $category = $_GET['category'];
+        $price = $_GET['price'];
+
+        $filteredItems = MedicalModel::filterItems($category, $price);
+
+        // Render the filtered items directly here
+        foreach ($filteredItems as $item) {
+            echo "<div>{$item['name']} - {$item['category']} - {$item['price']}</div>";
+        }
+    }
+}
