f32fec30c8c4d03caf5bbcadab7c95b3910fab06
Youssef <youssef1352019@icloud.com>
2023-12-20 01:40:02 +0200

    Edit confirmation page

--- views/confirmationpage.php
+++ views/confirmationpage.php
@@ -1,30 +1,41 @@
-<?php
+<?php 
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-include('../controllers/orderscontroller.php');
+require_once '../controllers/ordersController.php'; 
+require_once '../models/ordersmodel.php'; 
 
 // Set user ID in session and assign it to a variable
-$userid = $_SESSION['auth_user']['user_id'] ?? 0;
+$userid = $_SESSION['auth_user']['user_id'] ?? 0; 
 
 // create new order model
-$OrdersModel = new OrdersModel();
+$OrdersModel = new OrdersController();
 
-if ($row = $OrdersModel->getOrder($userid)) {
-    $orderid = $row["id"];
-    $user_name = $row["user_name"];
-    $city = $row["city"];
-    $address = $row["address"];
-    $order_date = $row["order_date"];
-    $status = $row["status"];
-    $total_price = $row["total_price"];
+$maxId = null;      
+$row=$OrdersModel->getOrder($userid);
+foreach ($row as $key) {
+    $orderId1 = $key['id'];
+
+    if ($maxId === null || $orderId1 > $maxId) {
+        $maxId = $orderId1;
+    }
+}
+$orderid=$maxId;
+
+
+if ($row=$OrdersModel->getOrderID($orderid)) {
+    $user_name=$row["user_name"];
+    $city=$row["city"];
+    $address=$row["address"];
+    $order_date=$row["order_date"];
+    $status=$row["status"];
+    $total_price=$row["total_price"];
 }
 
 ?>
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -32,64 +43,77 @@ if ($row = $OrdersModel->getOrder($userid)) {
     <link rel="stylesheet" href="../public/css/confirmationpage.css">
     <title>Document</title>
 </head>
-
 <body>
-    <div class="hero">
-        <center> 
-            <div class="container">
-                <div class="Left">
-                    <img class="check" src="../public/images/checked (1).png" alt="">
-                    <p class="message">Thank you, <?php echo $user_name ?> for your order!</p>
-                    <p class="message1">Your order will be delivered within 1 day of your purchase</p>
-                    <p class="message3">Your order number is</p>
-                    <p class="order_number">#<?php echo $orderid ?></p>
-                    <a href="index.php">
-                        <button>
-                            <span>Continue Shopping</span>
-                        </button>
-                    </a>
-                    <a href="receipt.php">
-                        <button class="B1">
-                            <span>Receipt</span>
-                        </button>
-                    </a>
-                </div>
-                <div class="right">
-                    <table style="width:100%">
-                        <tr>
-                            <td class="th">Name:</td>
-                            <td><?php echo $user_name ?></td>
-                        </tr>
-                        <tr>
-                            <td class="th">Order date:</td>
-                            <td><?php echo $order_date ?></td>
-                        </tr>
-                        <tr>
-                            <td class="th">City:</td>
-                            <td><?php echo $city ?></td>
-                        </tr>
-                        <tr>
-                            <td class="th">Address:</td>
-                            <td><?php echo $address ?></td>
-                        </tr>
-                        <tr>
-                            <td class="th">Status:</td>
-                            <td><?php echo $status ?></td>
-                        </tr>
-                        <tr>
-                            <td class="th">Total price:</td>
-                            <td><?php echo $total_price ?></td>
-                        </tr>
-                        <tr>
-                            <td class="th">Payment:</td>
-                            <td>Visa</td>
-                        </tr>
-                    </table>
-                </div>
-            </div>
-        </center>
-        <br>
+<div class="hero">
+    <?php include('../partials/navbar.php'); ?>
+    <center>
+    <div class="container">
+        <div class="Left">
+            <img class="check" src="../public/images/checked (1).png" alt="">
+            <p class="message">Thank you, <?php echo $user_name?> for your order!</p>
+            <p class="message1">Your order will be delivered within 1 day of your purchase</p>
+            <p class="message3">Your order number is</p>
+            <p class="order_number">#<?php echo $orderid?></p>
+            <a href="index.php">
+            <button>
+                <span>Continue Shopping</span>
+            </button>
+            </a>
+            <a href="receipt.php">
+            <button class="B1">
+                <span>Receipt</span>
+            </button>
+            </a>
+        </div>
+        <div class="right">
+            <table style="width:100%">
+            <tr>
+                <td class="th">Name:</td>
+                <td><?php echo $user_name?></td>
+            </tr>
+            <tr>
+                <td class="th">Order date:</td>
+                <td><?php echo $order_date?></td>
+            </tr>
+            <tr>
+                <td class="th">City:</td>
+                <td><?php echo $city?></td>
+            </tr>
+            <tr>
+                <td class="th">Address:</td>
+                <td><?php echo $address?></td>
+            </tr>
+            <tr>
+                <td class="th">Status:</td>
+                <td><?php echo $status?></td>
+            </tr>
+            <tr>
+                <td class="th">Total price:</td>
+                <td><?php echo $total_price?></td>
+            </tr>
+            <tr>
+                <td class="th">Payment:</td>
+                <td>Visa</td>
+            </tr>
+        </table>
+        </div>
     </div>
+    </center>
+    <br>
+</div>   
 </body>
-
 </html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
