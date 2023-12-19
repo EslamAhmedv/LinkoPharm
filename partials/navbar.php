50b1517d12f9db47f55cf0c423c532bb2e516acc
EslamAhmedv <125676587+EslamAhmedv@users.noreply.github.com>
2023-12-19 15:19:44 +0200

    dynamic menu for loggedin

--- partials/navbar.php
+++ partials/navbar.php
@@ -1,6 +1,8 @@
 <?php
 require_once("../controllers/UserController.php");
 require_once("../models/UserModel.php");
+require_once("../models/MenuModel.php");
+require_once("../includes/Dbh.php");
 require_once '../controllers/productscontroller.php';
 
 $userController = new UserController(new UserModel());
@@ -23,12 +25,11 @@ $userController = new UserController(new UserModel());
 // }
 
 
-if (isset($_GET['search'])){
-   $search=$_GET['search'];
-   $sql="select * from 'products' where name='$search'";
-   $result=mysqli_query($conn,$sql);
+if (isset($_GET['search'])) {
+   $search = $_GET['search'];
+   $sql = "select * from 'products' where name='$search'";
+   $result = mysqli_query($conn, $sql);
    return $result;
-   
 }
 
 
@@ -60,11 +61,9 @@ if (isset($_SESSION['auth_user'])) {
    // Debugging output
    var_dump($userRole);
 
-   // Check if the user role is set and is equal to 1 (adjust this condition based on your actual role values)
    if ($userRole !== null && $userRole == 1) {
-       // Redirect to the dashboard for users with role 1
-       header("Location: dashboard.php");
-       exit(); // Make sure to exit after sending the header
+      header("Location: dashboard.php");
+      exit();
    }
 }
 
@@ -83,170 +82,133 @@ if (isset($_SESSION['auth_user'])) {
 
 
 
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+   <meta charset="UTF-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/remixicon/3.5.0/remixicon.css">
+   <link rel="stylesheet" href="../views/search.php">
+   <link rel="stylesheet" href="../public/css/navbar.css">
+   <title>Your Page Title</title>
+</head>
+
+<body>
+
+   <?php if ($isUserLoggedIn) { ?>
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-      
-      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/remixicon/3.5.0/remixicon.css">
-
-      <link rel="stylesheet" href="../views/search.php">
-      
-      <link rel="stylesheet" href="../public/css/navbar.css">
-
- 
-
-   
-   <?php if ($isUserLoggedIn){?>
-    
-    
-
-   
       <header class="headerm" id="header">
          <nav class="nav container">
-         <img src="../public/images/logo.png" class="logom">
-      
-        
+            <img src="../public/images/logo.png" class="logom">
+
+
             <div class="nav__menu" id="nav-menu">
                <ul class="nav__list">
-                  <li class="nav__item">
-                 
-        
-    
-              
-                 
-             <a href="index.php" class="nav__link">Home</a>
-                  </li>
+               <?php
+                    // Fetch the menu items using MenuModel
+                    $menuModel = new MenuModel();
+                    $menuItems = $menuModel->fetchMenuItems();
 
-                  <li class="nav__item">
-                     <a href="products.php" class="nav__link">menu</a>
-                  </li>
-                  
-
-                  <li class="nav__item">
-                     <a href="wishlist.php" class="nav__link">wishlist</a>
-                  </li>
-
-                  <li class="nav__item">
-                     <a href="contactus.php" class="nav__link">contact us</a>
-                  </li>
-                  <li class="nav__item">
-                     <a href="aboutus.php" class="nav__link">about</a>
-                  </li>
-
-                  <li class="nav__item">
-                     <a href="logout.php" class="nav__link">log out</a>
-                  </li>
+                    foreach ($menuItems as $menuItem) {
+                        echo '<li class="nav__item">';
+                        echo '<a href="' . $menuItem['url'] . '" class="nav__link">' . $menuItem['label'] . '</a>';
+                        echo '</li>';
+                    }
+                    ?>
                </ul>
-               
-      
+
+
                <div class="nav__close" id="nav-close">
                   <i class="ri-close-line"></i>
                </div>
             </div>
 
             <div class="nav__actions">
-            <a href="userprofile.php"> <i class="ri-user-line nav__login" id="login-btn"></i></a>
-               
-            <button type="button" data-bs-toggle="modal" data-bs-target="#staticBackdrop">
+               <a href="userprofile.php"> <i class="ri-user-line nav__login" id="login-btn"></i></a>
+
+               <button type="button" data-bs-toggle="modal" data-bs-target="#staticBackdrop">
                   <i class="ri-search-line nav__search" id="search-btn"></i>
 
-            
-               <a href="checkout.php"><div class="fas fa-shopping-cart" id="cart-btn"></div></a>
-               
-               <div class="nav__toggle" id="nav-toggle">
-                  <i class="ri-menu-line"></i>
-               </div>
-             
+
+                  <a href="checkout.php">
+                     <div class="fas fa-shopping-cart" id="cart-btn"></div>
+                  </a>
+
+                  <div class="nav__toggle" id="nav-toggle">
+                     <i class="ri-menu-line"></i>
+                  </div>
+
             </div>
          </nav>
       </header>
 
       <div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
-    <div class="modal-dialog">
-        <div class="modal-content">
-            <form method="get" action="search_backend.php" id="searchForm">
-                <div class="modal-header">
-                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                </div>
-                <div class="modal-body">
-                    <input type="text" class="form-control" placeholder="search" name="search" id="searchInput">
-                    <div class="data-details" id="searchResults">
+         <div class="modal-dialog">
+            <div class="modal-content">
+               <form method="get" action="search_backend.php" id="searchForm">
+                  <div class="modal-header">
+                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                  </div>
+                  <div class="modal-body">
+                     <input type="text" class="form-control" placeholder="search" name="search" id="searchInput">
+                     <div class="data-details" id="searchResults">
                         <!-- Searched data will be displayed here -->
-                    </div>
-                </div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                    <button type="button" class="btn btn-primary" id="searchButton">Search</button>
-                </div>
-            </form>
-        </div>
-    </div>
-</div>
-               
-            
-               <a href="cart.php"><div class="fas fa-shopping-cart" id="cart-btn"></div></a>
-               
-               <div class="nav__toggle" id="nav-toggle">
-                  <i class="ri-menu-line"></i>
-               </div>
-             
+                     </div>
+                  </div>
+                  <div class="modal-footer">
+                     <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                     <button type="button" class="btn btn-primary" id="searchButton">Search</button>
+                  </div>
+               </form>
             </div>
-         </nav>
+         </div>
+      </div>
+
+
+      <a href="cart.php">
+         <div class="fas fa-shopping-cart" id="cart-btn"></div>
+      </a>
+
+      <div class="nav__toggle" id="nav-toggle">
+         <i class="ri-menu-line"></i>
+      </div>
+
+      </div>
+      </nav>
       </header>
 
-      
-      <!-- <div class="search" id="search">
-         <form action="search" class="search__form">
-            <i class="ri-search-line search__icon"></i>
-            <input type="text" placeholder="searching" class="search__input">
-         </form>
-
-         <i class="ri-close-line search__close" id="search-close"></i>
-      </div> -->
-      
-
-        
-            
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
       
 
-<?php } 
-     else{  ?>
-   <header class="headerm" id="header">
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+   <?php } else {  ?>
+      <header class="headerm" id="header">
          <nav class="nav container">
-         <img src="../public/images/logo.png" class="logom">
+            <img src="../public/images/logo.png" class="logom">
 
             <div class="nav__menu" id="nav-menu">
                <ul class="nav__list">
@@ -277,114 +239,117 @@ if (isset($_SESSION['auth_user'])) {
             </div>
 
             <div class="nav__actions">
-               
+
                <button type="button" data-bs-toggle="modal" data-bs-target="#staticBackdrop">
                   <i class="ri-search-line nav__search" id="search-btn"></i>
-                  
-</button>
-               
-<a href="login.php"> <i class="ri-user-line nav__login" id="login-btn"></i></a>
+
+               </button>
+
+               <a href="login.php"> <i class="ri-user-line nav__login" id="login-btn"></i></a>
                <!-- <a href="checkout.php"><div class="fas fa-shopping-cart" id="cart-btn"></div></a> -->
-               
+
                <div class="nav__toggle" id="nav-toggle">
                   <i class="ri-menu-line"></i>
                </div>
-             
+
             </div>
          </nav>
       </header>
 
-      
+
       <div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
-    <div class="modal-dialog">
-        <div class="modal-content">
-            <form method="get" action="search_backend.php" id="searchForm">
-                <div class="modal-header">
-                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                </div>
-                <div class="modal-body">
-                    <input type="text" class="form-control" placeholder="search" name="search" id="searchInput">
-                    <div class="data-details" id="searchResults">
+         <div class="modal-dialog">
+            <div class="modal-content">
+               <form method="get" action="search_backend.php" id="searchForm">
+                  <div class="modal-header">
+                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                  </div>
+                  <div class="modal-body">
+                     <input type="text" class="form-control" placeholder="search" name="search" id="searchInput">
+                     <div class="data-details" id="searchResults">
                         <!-- Searched data will be displayed here -->
-                    </div>
-                </div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                    <button type="button" class="btn btn-primary" id="searchButton">Search</button>
-                </div>
-            </form>
-        </div>
-    </div>
-</div>
+                     </div>
+                  </div>
+                  <div class="modal-footer">
+                     <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                     <button type="button" class="btn btn-primary" id="searchButton">Search</button>
+                  </div>
+               </form>
+            </div>
+         </div>
+      </div>
 
-      
-      
-  
-      <?php  } ?>
 
-      <!--=============== MAIN JS ===============-->
-      <script>/*=============== SHOW MENU ===============*/
-const navMenu = document.getElementById('nav-menu'),
-      navToggle = document.getElementById('nav-toggle'),
-      navClose = document.getElementById('nav-close')
 
-/* Menu show */
-navToggle.addEventListener('click', () =>{
-   navMenu.classList.add('show-menu')
-})
 
-/* Menu hidden */
-navClose.addEventListener('click', () =>{
-   navMenu.classList.remove('show-menu')
-})
+   <?php  } ?>
 
-/*=============== SEARCH ===============*/
-const search = document.getElementById('search'),
-      searchBtn = document.getElementById('search-btn'),
-      searchClose = document.getElementById('search-close')
+   <!--=============== MAIN JS ===============-->
+   <script>
+      /*=============== SHOW MENU ===============*/
+      const navMenu = document.getElementById('nav-menu'),
+         navToggle = document.getElementById('nav-toggle'),
+         navClose = document.getElementById('nav-close')
 
-/* Search show */
-searchBtn.addEventListener('click', () =>{
-   search.classList.add('show-search')
-})
+      /* Menu show */
+      navToggle.addEventListener('click', () => {
+         navMenu.classList.add('show-menu')
+      })
 
-/* Search hidden */
-searchClose.addEventListener('click', () =>{
-   search.classList.remove('show-search')
-})
+      /* Menu hidden */
+      navClose.addEventListener('click', () => {
+         navMenu.classList.remove('show-menu')
+      })
 
-/*=============== LOGIN ===============*/
-const login = document.getElementById('login'),
-      loginBtn = document.getElementById('login-btn'),
-      loginClose = document.getElementById('login-close')
+      /*=============== SEARCH ===============*/
+      const search = document.getElementById('search'),
+         searchBtn = document.getElementById('search-btn'),
+         searchClose = document.getElementById('search-close')
 
-/* Login show */
-loginBtn.addEventListener('click', () =>{
-   login.classList.add('show-login')
-})
+      /* Search show */
+      searchBtn.addEventListener('click', () => {
+         search.classList.add('show-search')
+      })
 
-/* Login hidden */
-loginClose.addEventListener('click', () =>{
-   login.classList.remove('show-login')
-})</script>
-<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-<script>
-    $(document).ready(function() {
-        $('#searchButton').on('click', function() {
+      /* Search hidden */
+      searchClose.addEventListener('click', () => {
+         search.classList.remove('show-search')
+      })
+
+      /*=============== LOGIN ===============*/
+      const login = document.getElementById('login'),
+         loginBtn = document.getElementById('login-btn'),
+         loginClose = document.getElementById('login-close')
+
+      /* Login show */
+      loginBtn.addEventListener('click', () => {
+         login.classList.add('show-login')
+      })
+
+      /* Login hidden */
+      loginClose.addEventListener('click', () => {
+         login.classList.remove('show-login')
+      })
+   </script>
+   <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+   <script>
+      $(document).ready(function() {
+         $('#searchButton').on('click', function() {
             var searchTerm = $('#searchInput').val(); // Get the search term
 
             $.ajax({
-                type: 'GET',
-                url: 'search_backend.php',
-                data: { search: searchTerm },
-                success: function(response) {
-                    $('#searchResults').html(response); // Update search results in the div
-                },
-                error: function() {
-                    $('#searchResults').html('Error occurred while processing the search.');
-                }
+               type: 'GET',
+               url: 'search_backend.php',
+               data: {
+                  search: searchTerm
+               },
+               success: function(response) {
+                  $('#searchResults').html(response); // Update search results in the div
+               },
+               error: function() {
+                  $('#searchResults').html('Error occurred while processing the search.');
+               }
             });
-        });
-    });
-</script>
-
+         });
+      });
+   </script>
